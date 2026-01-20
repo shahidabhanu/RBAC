9577f786e3ae4f677fafc9258989cefa2afd9afa
shahidabhanu <shahidabhanu162613@gmail.com>
2026-01-20 21:27:17 +0530

    Update chatbot logic, UI fixes, and caching improvements

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -1,28 +1,6 @@
 #auth.py
 
-# from datetime import datetime, timedelta
-# from jose import jwt, JWTError
-# from passlib.context import CryptContext
-# from backend.core.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# def hash_password(password):
-#     return pwd_context.hash(password)
-
-# def verify_password(plain, hashed):
-#     return pwd_context.verify(plain, hashed)
-
-# def create_access_token(data: dict):
-#     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     data.update({"exp": expire})
-#     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
-
-# def decode_access_token(token):
-#     try:
-#         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#     except JWTError:
-#         return None
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 from passlib.context import CryptContext
